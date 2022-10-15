8a75c1fa8000c0c110284a8456cfb0d1789d61e3
tenta <eunoia3802@gmail.com>
2022-10-15 22:12:54 +0900

    프로젝트 생성

---
+++ src/main/java/com/everybook/EverybookApplication.java
@@ -0,0 +1,13 @@
+package com.everybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverybookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverybookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everybook/everybook/EverybookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everybook.everybook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverybookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
